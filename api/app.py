c5256b5041481251782ec9443c0d579038b0c922
Bryan Rojas <bryroj14@gmail.com>
2020-02-05 14:35:56 -0800

    Updated wtih get all users endpoint from db

--- api/app.py
+++ api/app.py
@@ -1,7 +1,15 @@
-from flask import Flask, Response
+from flask import Flask, Response, jsonify
+from flask_mysqldb import MySQL
 import json
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'remotemysql.com'
+app.config['MYSQL_USER'] = 'ODK1LCc5DZ'
+app.config['MYSQL_PASSWORD'] = 'pN8S7PFHib'
+app.config['MYSQL_DB'] = 'ODK1LCc5DZ'
+
+mysql = MySQL(app)
 
 @app.route('/')
 def hello_world():
@@ -10,12 +18,18 @@ def hello_world():
     
 @app.route('/users/all', methods=['GET'])
 def get_all_users():
-    users = [
-        {"name": "Bryan Rojas", "email": "bryanrojascs@gmail.com"},
-        {"name": "David Ibarra", "email": "davidibarra@gmail.com"}
-    ]
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM ODK1LCc5DZ.Users;")
 
-    return Response(json.dumps({"users": users}), mimetype='application/json')
+    # https://stackoverflow.com/questions/29020839/mysql-fetchall-how-to-get-data-inside-a-dict-rather-than-inside-a-tuple
+    # These two lines zips the resulting values with cursor.description
+    # otherwise... you would have to do this manually like this:
+    # rows = cursor.fetchall()
+    # for row in rows:
+    #   id = row['id']
+    #   col1 = row['col1']
+    #   and so on...
+    columns = [col[0] for col in cur.description]
+    rows = [dict(zip(columns, row)) for row in cur.fetchall()]
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    return Response(json.dumps({"users": rows}), mimetype='application/json')
