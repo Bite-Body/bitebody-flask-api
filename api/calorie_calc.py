9fb8446e0d7e6a2c895ac7096cd446537dc433dc
Malik Coleman <malikai711@gmail.com>
2020-03-01 21:49:48 -0800

    updated user enpoint UI & calorie-calc recieves json

--- api/calorie_calc.py
+++ api/calorie_calc.py
@@ -34,7 +34,7 @@ def calc_cal():
             return(int(bmr_result))
 
         def calculate_activity(bmr_result): 
-            activity_level = input('What is your activity level (none, light, moderate, or high): ')
+            activity_level = request.get_json()['activity']
 
             if activity_level == 'none':
                 activity_level = 1.2 * bmr_result
@@ -48,23 +48,24 @@ def calc_cal():
             return(int(activity_level))
 
         def gain_or_lose(activity_level):
-            goals = input('Do you want to lose, maintain, or gain weight: ')
+            goals = request.get_json()['goal']
 
             if goals == 'lose':
                 calories = activity_level - 500
             elif goals == 'maintain':
                 calories = activity_level
             elif goals == 'gain':
-                gain = int(input('Gain 1 or 2 pounds per week? Enter 1 or 2: '))
+                gain = request.get_json()['gain']
                 if gain == 1: 
                     calories = activity_level + 500
                 elif gain == 2:
                     calories = activity_level + 1000
 
-            print('in order to ', goals, 'weight, your daily caloric goals should be', int(calories), '!')
+            
+            return int(calories)
 
 
-        gain_or_lose(calculate_activity(user_info()))
-        return Response(json.dumps({"Daily calorie to consume": 2000, "code": 200}), mimetype='application/json')
+        cal_count = gain_or_lose(calculate_activity(user_info()))
+        return Response(json.dumps({"Daily calorie to consume": cal_count, "code": 200}), mimetype='application/json')
     except Exception as e:
         return {"Error": "Unable to calculate calories."}
