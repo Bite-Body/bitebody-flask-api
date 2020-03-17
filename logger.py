dc909a87db55c308fecbb6a8ca21317851028e89
Bryan-Rojas <bryanrojascs@gmail.com>
2020-03-16 20:54:05 -0700

    Testing logging

--- logger.py
+++ logger.py
@@ -1,15 +1,22 @@
 import requests
 import datetime
+import json
 
 def post_log(action):
     url = 'https://tfn85zvwe5.execute-api.us-west-1.amazonaws.com/default/logging'
     current_time = datetime.datetime.now()
     log = {
-        "log": {
-            "source": "Bitebody.xyz API",
-            "time": str(current_time),
-            "action": action
-            }
+        'log': {
+            'source': 'Bitebody.xyz API',
+            'time': str(current_time),
+            'action': action
         }
+    }
 
-    requests.post(url, log)
+    print(json.dumps(log))
+
+    try:
+        r = requests.get(url, data=json.dumps(log))
+        print(r.text, r.status_code, r.reason)
+    except Exception as e:
+        print(e)
