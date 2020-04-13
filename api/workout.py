7f31dc15b01aac5d23feb85abf9dba513c78bc10
Hector Mendoza <mendoza.hector_23@yahoo.com>
2020-04-12 20:11:18 -0700

    added SQL statement changes from DB update

--- api/workout.py
+++ api/workout.py
@@ -17,18 +17,21 @@ def get_all_workouts():
             temp_workout = {}
             temp_workout['id'] = row[0]
             temp_workout['workout_name'] = row[1] 
-            temp_workout['workout_description'] = row[2]
-            temp_workout['workout_image_path'] = row[3]
-            temp_workout['genre'] = row[4]
-            temp_workout['body_part'] = row[5]
-            temp_workout['duration'] = row[6]
+            temp_workout['main_muscle_group'] = row[2]
+            temp_workout['detailed_muscle_group'] = row[3]
+            temp_workout['other_muscle_groups'] = row[4]
+            temp_workout['type'] = row[5]
+            temp_workout['mechanics'] = row[6]
             temp_workout['equipment'] = row[7]
+            temp_workout['difficulty'] = row[8]
+            temp_workout['exercise_steps'] = row[9]
+            temp_workout['image_path'] = row[10]
             all_workouts.append(temp_workout)
 
         post_log('GET /workouts/all')
         return Response(json.dumps({"workouts": all_workouts, "code": 201}), mimetype='application/json')
     except Exception as e:
-        return {"Error": "Unable to retrive all workouts."}
+        return {"Error": "Unable to retrive all workouts.", "ErrorMessage": str(e)}
 
 @workout.route('/<int:wkoutID>', methods=['GET'])
 def find_workout(wkoutID):
@@ -39,19 +42,22 @@ def find_workout(wkoutID):
         workout = {
             'id' : row[0],
             'workout_name':row[1],
-            'workout_description':row[2],
-            'workout_image_path' : row[3],
-            'genre': row[4],
-            'body_part': row[5],
-            'duration': row[6],
-            'equipment': row[7]
+            'main_muscle_group':row[2],
+            'detailed_muscle_group' : row[3],
+            'other_muscle_group': row[4],
+            'type': row[5],
+            'mechanics': row[6],
+            'equipment': row[7],
+            'difficulty': row[8],
+            'exercise_steps': row[9],
+            'image_path': row[10]
         }
 
         post_log('GET /workouts/<int:wkoutID>')
         return Response(json.dumps({"workout": workout, "code": 200}), mimetype='application/json')
     except Exception as e:
 
-        return {"Error": "Unable to retrieve this workout."}
+        return {"Error": "Unable to retrieve this workout.", "ErrorMessage": str(e)}
 
 @workout.route('/<int:wkoutID>', methods=['DELETE'])
 def delete_workout(wkoutID):
@@ -66,39 +72,45 @@ def delete_workout(wkoutID):
         post_log('DELETE /workouts/<int:wkoutID>')
         return Response(json.dumps({"deleted": deleted, "code": 200}), mimetype='application/json')
     except Exception as e:
-        return {"Error": "Unable to delete this workout."}
+        return {"Error": "Unable to delete this workout.", "ErrorMessage": str(e)}
 
 @workout.route('/<int:wkoutID>', methods = ['PUT'])
 def update_workout_info(wkoutID):
     try:
         cur = mysql.connection.cursor()
         workout_name = request.get_json()['workout_name']
-        workout_description = request.get_json()['workout_description']
-        workout_image_path = request.get_json()['workout_image_path']
-        genre = request.get_json()['genre']
-        body_part = request.get_json()['body_part']
-        duration = request.get_json()['duration']
+        main_muscle_group = request.get_json()['main_muscle_group']
+        detailed_muscle_group = request.get_json()['detailed_muscle_group']
+        other_muscle_groups = request.get_json()['other_muscle_groups']
+        type_ = request.get_json()['type']
+        mechanics = request.get_json()['mechanics']
         equipment = request.get_json()['equipment']
+        difficulty = request.get_json()['difficulty']
+        excercise_steps = request.get_json()['excercise_steps']
+        image_path = request.get_json()['image_path']
             
-        cur.execute("UPDATE BiteBody.Workouts SET workout_name = '"+str(workout_name) + "',workout_description = '" + str(workout_description)+ "',workout_image_path = '"+ 
-        str(workout_image_path)+"',genre = '"+ str(genre) + "',body_part = '" + str(body_part)+"',duration = '" + str(duration)+"',equipment = '" + str(equipment)+
+        cur.execute("UPDATE BiteBody.Workouts SET workout_name = '"+str(workout_name) + "',main_muscle_group = '" + str(main_muscle_group)+ "',detailed_muscle_group = '"+ 
+        str(detailed_muscle_group)+"',other_muscle_groups = '"+ str(other_muscle_groups) + "',type = '" + str(type_)+"',mechanics = '" + str(mechanics)+"',equipment = '" + str(equipment)+ "',difficulty = '" + str(difficulty)+ "',excercise_steps = '" + str(excercise_steps)+ "',image_path = '" + str(image_path)+
         "'WHERE id = "+ str(wkoutID)+";")
         mysql.connection.commit()
         workout = { 
-            'id': wkoutID,
+            'id' : wkoutID,
             'workout_name': workout_name,
-            'workout_description': workout_description,
-            'workout_image_path' : workout_image_path,
-            'genre': genre,
-            'body_part': body_part,
-            'duration': duration,
-            'equipment': equipment
+            'main_muscle_group': main_muscle_group,
+            'detailed_muscle_group' : detailed_muscle_group,
+            'other_muscle_groups': other_muscle_groups,
+            'type': type_,
+            'mechanics': mechanics,
+            'equipment': equipment,
+            'difficulty': difficulty,
+            'excercise_steps': excercise_steps,
+            'image_path': image_path
         }
 
         post_log('PUT /workouts/<int:wkoutID>')
         return Response(json.dumps({"updated": workout, "code": 201}), mimetype='application/json')
     except Exception as e:
-        return {"Error": "Unable to update this workout."}
+        return {"Error": "Unable to update this workout.", "ErrorMessage": str(e)}
 
 @workout.route('', methods=['POST'])
 def insert_workout():
@@ -106,34 +118,43 @@ def insert_workout():
         cur = mysql.connection.cursor()
         id = request.get_json()['id']
         workout_name = request.get_json()['workout_name']
-        workout_description = request.get_json()['workout_description']
-        workout_image_path = request.get_json()['workout_image_path']
-        genre = request.get_json()['genre']
-        body_part = request.get_json()['body_part']
-        duration = request.get_json()['duration']
+        main_muscle_group = request.get_json()['main_muscle_group']
+        detailed_muscle_group = request.get_json()['detailed_muscle_group']
+        other_muscle_groups = request.get_json()['other_muscle_groups']
+        type_ = request.get_json()['type']
+        mechanics = request.get_json()['mechanics']
         equipment = request.get_json()['equipment']
-        cur.execute("INSERT INTO BiteBody.Workouts (id, workout_name, workout_description, workout_image_path, genre, body_part, duration, equipment) VALUES ('" 
+        difficulty = request.get_json()['difficulty']
+        excercise_steps = request.get_json()['excercise_steps']
+        image_path = request.get_json()['image_path']
+        cur.execute("INSERT INTO BiteBody.Workouts (id, workout_name, main_muscle_group, detailed_muscle_group, other_muscle_groups, type, mechanics, equipment, difficulty, excercise_steps,image_path) VALUES ('" 
             + id + "', '" 
             + workout_name + "', '" 
-            + workout_description + "', '" 
-            + workout_image_path + "', '"
-            + genre + "', '"  
-            + body_part + "', '" 
-            + duration + "', '" 
-            + equipment + "');")
+            + main_muscle_group + "', '" 
+            + detailed_muscle_group + "', '"
+            + other_muscle_groups + "', '"  
+            + type_ + "', '"
+            + mechanics + "', '"
+            + equipment + "', '"
+            + difficulty + "', '" 
+            + excercise_steps + "', '" 
+            + image_path + "');")
         mysql.connection.commit()
         workout = { 
             'id': id,
             'workout_name': workout_name,
-            'workout_description': workout_description,
-            'workout_image_path' : workout_image_path,
-            'genre': genre,
-            'body_part': body_part,
-            'duration': duration,
-            'equipment': equipment
+            'main_muscle_group': main_muscle_group,
+            'detailed_muscle_group' : detailed_muscle_group,
+            'other_muscle_groups': other_muscle_groups,
+            'type': type_,
+            'mechanics': mechanics,
+            'equipment': equipment,
+            'difficulty': difficulty,
+            'excercise_steps': excercise_steps,
+            'image_path': image_path
         }
 
         post_log('POST /workouts')
         return Response(json.dumps({"workout added": workout, "code": 201}), mimetype='application/json')
     except Exception as e:
-        return {"Error": "Unable to create this workout."}
+        return {"Error": "Unable to create this workout.", "ErrorMessage":str(e)}
